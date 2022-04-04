9e7d53c9a04f0a70b57714d3c9d6d679fda13070
unknown <aisa.smajic27@gmail.com>
2022-04-05 01:49:38 +0200

    zadatak 2

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,8 +1,9 @@
 // Write code below this line
 function functionPrintInfo(name, age){
-console.log("John", 23);
-console.log("Michael", 43);
+console.log(name, age);
 }
-functionPrintInfo();
+functionPrintInfo("John", 23);
+functionPrintInfo("Michael", 43);
 // Write code above this line
+
 module.exports = functionPrintInfo;
